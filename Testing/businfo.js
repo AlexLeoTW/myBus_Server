a4e2fe92c642a20438817f625b6d8c8cc487ad45
AlexLeoTW <alex@alexleo.click>
2016-04-17 23:08:15 +0800

    update businfo.js

--- Testing/businfo.js
+++ Testing/businfo.js
@@ -1,5 +1,31 @@
-function getBusInfo(plate_no, mysql, db_column) {
-    mysql.connect();
-    
-    mysql.end();
+function sendRoutelist(res, connection) {
+    connection.query('SELECT * FROM route', function (err, rows) {
+        if (err) { throw err; }
+        res.send(JSON.stringify(rows));
+    });
 }
+
+function sendBusInfo(res, plate_no, connection) {
+    connection.query('SELECT * FROM bus WHERE plate_no ==' + plate_no, function(err, rows) {
+        res.send();
+    });
+    connection.release();
+}
+
+
+/*
+    raw
+        plate_no, longitude, latitude, timestamp
+
+    data_cache
+        plate_no, seg_location, seg_time
+
+    time
+        plate_no, route, stop, elapseTime
+
+    route
+        no, name, from, to
+
+    stop
+        no, route, name
+*/
